dca95c3dae350725ba89f824c4d45829272a595a
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-01-28 11:08:56 +0000

    proyecto finish?

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.es', '.net'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adj.length; j++){
+    for(let k = 0; k < noun.length; k++){
+      for(let h = 0; h < extension.length; h++){
+        let domain = pronoun[i] + adj[j] + noun[k] + extension[h];
+        console.log(domain);
+      }
+    }
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
